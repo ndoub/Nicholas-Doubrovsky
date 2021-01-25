f780ba2ab0e81ed96e49b1ce0552db634f8b1c6e
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-01-25 14:47:21 -0800

    Added comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,14 @@
 
 namespace Mine.Models
 {
+    //Items for characters and monsters to use
     public class ItemModel
     {
+        //The Id for the Item
         public string Id { get; set; }
+        //The Display Text for the item
         public string Text { get; set; }
+        //The Description for the Item
         public string Description { get; set; }
 
         //The Value of the Item Damage
