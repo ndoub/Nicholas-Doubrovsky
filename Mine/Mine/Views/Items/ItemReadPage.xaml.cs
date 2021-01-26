be9a09d5a8492648e1d383a399ce3e0ca7d10430
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-01-25 16:50:34 -0800

    Renamed ItemDetailPage to ItemReadPage, updated calling files. Changes to ItemReadPage.xaml, ItemReadpage.xaml.cs, ItemsPage.xaml.cs, Mine.csproj

--- Mine/Mine/Views/Items/ItemReadPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemReadPage.xaml.cs
@@ -11,18 +11,18 @@ namespace Mine.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class ItemDetailPage : ContentPage
+    public partial class ItemsReadPage : ContentPage
     {
         ItemDetailViewModel viewModel;
 
-        public ItemDetailPage(ItemDetailViewModel viewModel)
+        public ItemsReadPage(ItemDetailViewModel viewModel)
         {
             InitializeComponent();
 
             BindingContext = this.viewModel = viewModel;
         }
 
-        public ItemDetailPage()
+        public ItemsReadPage()
         {
             InitializeComponent();
 
