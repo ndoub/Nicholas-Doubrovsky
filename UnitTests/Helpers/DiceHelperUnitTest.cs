6b9b43c3c154ba5150c21dd0800651ac168ed55c
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-02-09 14:41:14 -0800

    Added Invalid zero test to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -2,9 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Mine.Models;
+using NUnit.Framework;
+using Mine.Helpers;
+
 namespace UnitTests.Helpers
 {
-    class DiceHelperUnitTest
+    [TestFixture]
+    public class DiceHelperUnitTest
     {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            //Arrange
+
+            //Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
