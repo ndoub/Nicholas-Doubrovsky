160ff10690c7fd65660d5d14d46acfe669259e3d
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-02-09 13:58:23 -0800

    Added a test to ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
+using NUnit.Framework;
+
 namespace UnitTests.Models
 {
+    [TestFixture]
     class ItemModelTests
     {
+        [Test]
+        public void ItemModel_Constructor_Valid()
+        {
+            //Arrange
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.Pass();
+        }
     }
 }
